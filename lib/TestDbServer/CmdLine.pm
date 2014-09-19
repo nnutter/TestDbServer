c82d33bd66d93ffa2d11ee7c60c5cfd10b082ee5
Anthony Brummett <abrummet@genome.wustl.edu>
2014-09-19 15:27:28 -0500

    move get_template_name_from_id to the CmdLine module

--- lib/TestDbServer/CmdLine.pm
+++ lib/TestDbServer/CmdLine.pm
@@ -7,12 +7,14 @@ use File::Basename;
 use LWP;
 use Carp;
 use URI::Escape qw(uri_escape);
+use JSON qw(decode_json);
 
 use strict;
 use warnings;
 
 use Exporter 'import';
-our @EXPORT_OK = qw(get_user_agent url_for assert_success template_id_from_name database_id_from_name);
+our @EXPORT_OK = qw(get_user_agent url_for assert_success template_id_from_name database_id_from_name
+                    get_template_name_from_id);
 
 sub find_available_sub_command_paths {
     my($cmd) = shift;
@@ -79,6 +81,22 @@ sub assert_success {
         Carp::croak('Got error response '.$rsp->code . ': '. $rsp->message);
     }
     return 1;
+}
+
+sub get_template_name_from_id {
+    my($template_id) = @_;
+
+    return undef unless defined $template_id;
+
+    my $ua = get_user_agent();
+
+    my $req = HTTP::Request->new(GET => url_for('templates',$template_id));
+    my $rsp = $ua->request($req);
+    unless (eval { assert_success($rsp); 1 }) {
+        return undef;
+    }
+    my $tmpl = decode_json($rsp->content);
+    return $tmpl->{name};
 }
 
 1;

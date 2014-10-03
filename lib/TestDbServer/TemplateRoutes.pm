3225e6587b88922a87ecf0b084e6097200283430
Anthony Brummett <abrummet@genome.wustl.edu>
2014-10-03 13:30:32 -0500

    additional logging for database and template routes

--- lib/TestDbServer/TemplateRoutes.pm
+++ lib/TestDbServer/TemplateRoutes.pm
@@ -13,6 +13,12 @@ sub list {
     my $self = shift;
 
     my $params = $self->req->params->to_hash;
+
+    $self->app->log->info('list templates: '
+                            . %$params
+                                ? join(', ', map { join(' => ', $_, $params->{$_}) } keys %$params )
+                                : 'no params');
+
     my $templates = %$params
                     ? $self->app->db_storage->search_template(%$params)
                     : $self->app->db_storage->search_template;
@@ -33,10 +39,17 @@ sub list {
         ) {
             %render_args = ( status => 400, text => $1 );
         } else {
+            $self->app->log->fatal("list templates exception: $_");
             die $_;
         }
     }
     finally {
+        if (exists($render_args{status}) and $render_args{status} == 400) {
+            $self->app->log->error("list templates failed: $render_args{text}");
+        } else {
+            $self->app->log->info('found ' . scalar($render_args{json}) . ' templates');
+        }
+
         $self->render(%render_args);
     }
 }
@@ -45,13 +58,18 @@ sub get {
     my $self = shift;
 
     my $id = $self->stash('id');
+
+    $self->app->log->info("get template $id");
+
     my $schema = $self->app->db_storage;
 
     my $template = $schema->find_template($id);
     if ($template) {
+        $self->app->log->info("found template $id");
         my %template = map { $_ => $template->$_ } qw(template_id name owner note sql_script create_time last_used_time);
         $self->render(json => \%template);
     } else {
+        $self->app->log->info("template $id not found");
         $self->render_not_found;
     }
 }
@@ -59,10 +77,12 @@ sub get {
 sub save {
     my $self = shift;
 
-    if ($self->req->param('based_on')) {
-        $self->_save_based_on();
+    if (my $database_id = $self->req->param('based_on')) {
+        $self->app->log->info("create template based on database $database_id");
+        $self->_save_based_on($database_id);
 
     } else {
+        $self->app->log->info('uploading template from file');
         $self->_save_file();
     }
 }
@@ -91,6 +111,7 @@ sub _save_file {
             ||
             (ref($_) && $_->isa('DBIx::Class::Exception') && $_ =~ m/unique constraint/i)
         ) {
+            $self->app->log->error('save_file conflict');
             $return_code = 409;
         } else {
             $self->app->log->error("save_file: $_");
@@ -99,6 +120,7 @@ sub _save_file {
     };
 
     if ($template_id) {
+        $self->app->log->info('template uploaded');
         my $response_location = TestDbServer::Utils::id_url_for_request_and_entity_id($self->req, $template_id);
         $self->res->headers->location($response_location);
     }
@@ -129,22 +151,26 @@ sub _save_based_on {
     }
     catch {
         if (ref($_) && $_->isa('Exception::RequiredParamMissing')) {
+            $self->app->log->error('Missing required parameter: '.join(', ', @{ $_->params }));
             $return_code = 400;
 
         } elsif (ref($_) && $_->isa('Exception::DatabaseNotFound')) {
+            $self->app->log->error("database not found: ".$self->param('based_on'));
             $return_code = 404;
 
         } elsif (ref($_) && $_->isa('DBIx::Class::Exception') && m/UNIQUE constraint failed: db_template\.name/) {
+            $self->app->log->error('there is already a template with that name');
             $return_code = 409;
 
         } else {
-            $self->app->log->error("create template based on: $_");
+            $self->app->log->fatal("create template based on: $_");
             die $_;
         }
 
     };
 
     if ($template_id) {
+        $self->app->log->info("Created template $template_id");
         my $response_location = TestDbServer::Utils::id_url_for_request_and_entity_id($self->req, $template_id);
         $self->res->headers->location($response_location);
     }
@@ -155,6 +181,8 @@ sub delete {
     my $self = shift;
 
     my $id = $self->stash('id');
+    $self->app->log->info("deleting template $id");
+
     my $schema = $self->app->db_storage;
 
     my $return_code;
@@ -170,9 +198,10 @@ sub delete {
     }
     catch {
         if (ref($_) && $_->isa('Exception::TemplateNotFound') || $_->isa('Exception::CannotUnlinkFile')) {
+            $self->app->log->error("template $id does not exist");
             $return_code = 404;
         } else {
-            $self->app->log->error("_create_database_from_template: $_");
+            $self->app->log->fatal("_create_database_from_template: $_");
             die $_;
         }
     };

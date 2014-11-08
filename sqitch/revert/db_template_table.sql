0db1be1f999f7be647fb1902ceb82240af47e0ca
Nathaniel Nutter <iam@nnutter.com>
2014-11-08 14:28:20 -0600

    didn't commit this earlier

--- sqitch/revert/db_template_table.sql
+++ sqitch/revert/db_template_table.sql
@@ -1,7 +1,15 @@
--- Revert db_template_table
+-- Deploy db_template_table
 
 BEGIN;
 
-drop table db_template;
+CREATE TABLE IF NOT EXISTS db_template (
+            template_id SERIAL NOT NULL PRIMARY KEY,
+            name VARCHAR NOT NULL UNIQUE,
+            owner VARCHAR NOT NULL,
+            note VARCHAR,
+            sql_script TEXT NOT NULL,
+            create_time TIMESTAMP NOT NULL DEFAULT(now()),
+            last_used_time TIMESTAMP NOT NULL DEFAULT(now())
+        );
 
 COMMIT;
